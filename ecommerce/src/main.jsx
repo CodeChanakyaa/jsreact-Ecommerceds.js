a1669e5da598fc1e191b245ca9fcb74a1675c395
Code Chanakya <123615985+CodeChanakyaa@users.noreply.github.com>
2024-01-08 14:46:44 +0530

    Update main.jsx

--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -6,8 +6,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="dev-08cx4fxtf51boa1g.us.auth0.com"
-    clientId="2mCfDdukHV6ZVBf4RU8ONwFoEqor9QNJ"
+    domain=""
+    clientId=""
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
